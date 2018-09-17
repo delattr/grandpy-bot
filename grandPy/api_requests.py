63685f7a1f40f4faec3b43416a55e5800883bdf6
elenoko <elenoko@gmail.com>
2018-09-17 15:40:18 +0200

    Functions for http requests

--- grandPy/api_requests.py
+++ grandPy/api_requests.py
@@ -42,20 +42,21 @@ def getDetail(place_id, key):
     url = 'https://maps.googleapis.com/maps/api/place/details/json'
     payload = {'key': key,
                'placeid': place_id,
-               'fields': 'address_components'}
+               'fields': 'address_components,name'}
     try:
         req = requests.get(url, params=payload)
         response = req.json()
         req.raise_for_status()
 
         address_components = response['result']['address_components']
+        print(address_components)
         for i, v in enumerate(address_components):
 
             if v['types'][0] == 'route':
                 route = address_components[i]['long_name']
                 break
             else:  # Get first element from address_compnnents
-                route = address_components[0]['long_name']
+                route = response['result']['name']
         # Search info from wiki using route name
 
         return route
@@ -74,14 +75,21 @@ def wiki_search(text):
         "generator": "search",
         "utf8": 1,
         "inprop": "url",
-        "exchars": "1200",
+        "exchars": "500",
         "gsrsearch": text,
         "gsrnamespace": "0",
         "gsrlimit": "1"
+        # "explaintext": 1
+        # "exsectionformat": "plain"
     }
 
     req = requests.get(url, params=payload)
     result = req.json()
-    val = result['query']['pages'].values()
-    data =list(val)
-    return data[0]
+    print(result)
+    for k, v in result['query']['pages'].items():
+        if k == -1:
+            response = 'no results found'
+        else:
+            data = v
+            response = f'{data["extract"]} <a href = "{data["fullurl"]}">[Wikipedia]</a>'
+    return response
