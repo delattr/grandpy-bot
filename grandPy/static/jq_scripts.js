ff7d593c5c4d5e94d82ddd854c5ec9d6768eb430
elenoko <elenoko@gmail.com>
2018-09-17 15:42:37 +0200

    Js and Jquery for post ajax and displaying results

--- grandPy/static/jq_scripts.js
+++ grandPy/static/jq_scripts.js
@@ -1,26 +1,46 @@
 $(document).ready(function () {
     var counter = 0;
     var logId;
+    // loadSpinner();
 
     function postAjax(url, data, callback) {
+        loadSpinner();
         $.ajax({
             type: "POST",
             url: url,
             data: data,
             success: callback,
             error: (xhr) => {
-                var msg = 'ERROR: ' + xhr.status + ' ' + xhr.statusText
-                chatOutput('justify-content-start', 'primary', msg)
+                var msg = 'ERROR: ' + xhr.status + ' ' + xhr.statusText;
+                chatOutput('grandpy', msg);
+            },
+            complete: function () {
+                $('.lds-ellipsis').hide();
             }
         });
     }
 
-    function chatOutput(justify, color, text) {
+
+
+    function loadSpinner() {
+
+        var loading = '<div></div><div></div><div></div>'
+        chatOutput('lds-ellipsis', loading);
+    }
+    // Append speechballoon inside of chatting box
+    function chatOutput(clss, text) {
         counter++;
         logId = "log_" + counter;
+        var justify;
+        if (clss === 'grandpy' || clss === 'lds-ellipsis') {
+            justify = 'justify-content-start'
+        } else {
+            justify = 'justify-content-end'
+        };
+
         var flex = $("<div>").attr("class", `d-flex ${justify}`);
         var output = $("<div>").attr({
-            class: `m-2 p-2 rounded shadow text-white bg-${color}`,
+            class: `m-2 p-2 rounded shadow-sm ${clss}`,
             id: logId
         });
         output.html(text);
@@ -28,12 +48,11 @@ $(document).ready(function () {
         $(flex).append(output)
         $("#chatlog").append(flex);
         $("#chatlog").scrollTop($('#chatlog')[0].scrollHeight);
-
     }
 
-
+    // Display google maps object
     function createMap(location) {
-        chatOutput('justify-content-start', 'success', "");
+        chatOutput('grandpy', "");
         $("#" + logId).css({
             width: '400px',
             height: '300px'
@@ -51,34 +70,48 @@ $(document).ready(function () {
         });
 
     }
-
+    // Submit event
     $("form").submit(function (event) {
         event.preventDefault();
 
         var userInput = $("input").val();
         if (userInput != null) {
-            chatOutput('justify-content-end', 'success', userInput);
+            chatOutput('user', userInput);
         };
-
-        postAjax('/post', $("form").serialize(), function (response) {
-            if (response.status == 'OK') {
-
-
-                chatOutput('justify-content-start', 'primary', response.address);
-
-                createMap(response.location);
-
-                chatOutput('justify-content-start', 'primary', response.route + '<br>' + response.wiki.extract);
-
-
-
-            } else {
-                chatOutput('justify-content-start', 'primary', response.status);
-            }
-        });
-
+        // post FormData to the server
+        postAjax('/post', $("form").serialize(), callback);
+        // Clear input field
         $("form input").val("");
     });
+    // Ajax callback function
+    function callback(response) {
+        if (response.status == 'OK') {
+            var promise = loader(chatOutput);
+            promise.then(function () {
+                chatOutput('grandpy', response.address)
+                return loader()
+            }).then(function () {
+                createMap(response.location)
+                return loader()
+            }).then(function () {
+                chatOutput('grandpy', response.wiki)
+            });
 
+        } else {
+            loader().then(function () {
+                chatOutput('grandpy', response.status);
+            });
+        }
+
+        function loader() {
+            return new Promise(function (resolve) {
+                loadSpinner();
+                setTimeout(() => {
+                    $('.lds-ellipsis').hide()
+                    resolve();
+                }, 1000);
+            });
+        }
+    }
 
 });
