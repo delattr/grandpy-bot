6997653975edba872d139e0a15d9566c884a25d9
elenoko <elenoko@gmail.com>
2019-04-14 20:19:03 +0200

    configuration file

--- config.py
+++ config.py
@@ -1,2 +1,20 @@
-SECRET_KEY = 'f03a6f77b74d40f21ff0b82cd24b59ea'
+import os
+
+
+class BaseConfig(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'default')
+    MAPS_API_KEY = os.environ.get('MAPS_API_KEY')
+    PLACE_API_KEY = os.environ.get('PLACE_API_KEY')
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+    TESTING = False
+
 
